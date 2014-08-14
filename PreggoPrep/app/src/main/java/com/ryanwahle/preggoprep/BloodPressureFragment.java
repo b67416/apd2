7ca65ca9bac6859a869b17bde5e68e54784002b7
Ryan Wahle <ryanwahle@fullsail.edu>
2014-08-13 22:16:33 -0700

    added fragments and nav drawer functionality

---
+++ PreggoPrep/app/src/main/java/com/ryanwahle/preggoprep/BloodPressureFragment.java
@@ -0,0 +1,55 @@
+package com.ryanwahle.preggoprep;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ryanwahle on 8/13/14.
+ */
+public class BloodPressureFragment extends Fragment {
+
+
+        /**
+         * A placeholder fragment containing a simple view.
+         */
+
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static BloodPressureFragment newInstance(int sectionNumber) {
+            BloodPressureFragment fragment = new BloodPressureFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public BloodPressureFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_blood_pressure, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+
+    }
--- PreggoPrep/app/src/main/java/com/ryanwahle/preggoprep/NavigationDrawerFragment.java
+++ PreggoPrep/app/src/main/java/com/ryanwahle/preggoprep/NavigationDrawerFragment.java
@@ -102,9 +102,10 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        getString(R.string.title_appointments),
+                        getString(R.string.title_blood_pressure),
+                        getString(R.string.title_kick_times),
+                        getString(R.string.title_contractions),
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
@@ -234,10 +235,10 @@ public class NavigationDrawerFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // If the drawer is open, show the global app actions in the action bar. See also
         // showGlobalContextActionBar, which controls the top-left area of the action bar.
-        if (mDrawerLayout != null && isDrawerOpen()) {
-            inflater.inflate(R.menu.global, menu);
-            showGlobalContextActionBar();
-        }
+       // if (mDrawerLayout != null && isDrawerOpen()) {
+      //      inflater.inflate(R.menu.global, menu);
+       //     showGlobalContextActionBar();
+      //  }
         super.onCreateOptionsMenu(menu, inflater);
     }
 
@@ -247,10 +248,10 @@ public class NavigationDrawerFragment extends Fragment {
             return true;
         }
 
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
+      //  if (item.getItemId() == R.id.action_example) {
+      //      Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+      //      return true;
+      //  }
 
         return super.onOptionsItemSelected(item);
     }
