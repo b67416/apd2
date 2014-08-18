10a0ab078e9688b1570832a7d7d3d93451cc8823
Ryan Wahle <ryanwahle@fullsail.edu>
2014-08-17 23:40:13 -0700

    added blood pressure layout for milestone 2

--- PreggoPrep/app/src/main/java/com/ryanwahle/preggoprep/BloodPressureFragment.java
+++ PreggoPrep/app/src/main/java/com/ryanwahle/preggoprep/BloodPressureFragment.java
@@ -4,52 +4,57 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * Created by ryanwahle on 8/13/14.
- */
 public class BloodPressureFragment extends Fragment {
 
+    private static final String ARG_SECTION_NUMBER = "section_number";
 
-        /**
-         * A placeholder fragment containing a simple view.
-         */
+    public static BloodPressureFragment newInstance(int sectionNumber) {
+        BloodPressureFragment fragment = new BloodPressureFragment();
+        Bundle args = new Bundle();
 
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static BloodPressureFragment newInstance(int sectionNumber) {
-            BloodPressureFragment fragment = new BloodPressureFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public BloodPressureFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_blood_pressure, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-
+        return fragment;
     }
+
+    public BloodPressureFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_blood_pressure, container, false);
+
+        setHasOptionsMenu(true);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    // Inflate Action Bar Items
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_blood_pressure, menu);
+    }
+
+    // When user clicks the Action Bar Item
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO: Add the new entry logic
+
+        Toast.makeText(getActivity().getApplicationContext(), "<TEMPORARY> Add new blood pressure recording", Toast.LENGTH_LONG).show();
+
+        return true;
+    }
+}
